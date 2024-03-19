10dddab658562cb44d5cb76a0f14f8569e160f37
odziov <mrtdzv@gmail.com>
2024-03-19 21:41:10 +0300

    поправил

--- start.sh
+++ start.sh
@@ -19,7 +19,7 @@ if [ ! -f $VIRTUALENV/bin/pip ]; then
 fi
 
 # Install the requirements
-$VIRTUALENV/bin/pip install -r requirements.txt
+$VIRTUALENV/bin/python3 -m pip install --no-cache-dir -r requirements.txt
 
 # Run your glorious application
 $VIRTUALENV/bin/python3 server.py
